3cce5a1460d34e710bf2e404c0711b9c912fa037
Vineeth Puli <vpuli98@gmail.com>
2017-09-26 22:03:53 -0400

    Created Finstagram

--- Finstagram/ViewController.swift
+++ Finstagram/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitleLabel: UILabel!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        if userNameTextField.hasText {
+            print(userNameTextField.text!)
+        } else {
+            appTitleLabel.text = "Enter a Username"
+        }
+        
+    }
+    
 }
 
